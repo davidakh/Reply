ea85f6b9dac6da76a393a80c1baa045e3874dfe9
David <david.akhmedbayev@icloud.com>
2026-01-05 15:46:49 -0500

    Initial Commit

---
+++ Reply/ReplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReplyApp.swift
+//  Reply
+//
+//  Created by David Akhmedbayev on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ReplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
